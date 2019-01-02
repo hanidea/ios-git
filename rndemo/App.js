8c14191af76f7259deb78de9cb94cb33b29a1630
hanidea <hydavid@126.com>
2019-01-02 16:38:56 +0800

    rndemo
    navmenu

--- rndemo/App.js
+++ rndemo/App.js
@@ -7,8 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -18,12 +18,57 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.state={
+      selectedTab:'tb_polular'
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_polular'}
+            selectedTitleStyle={{color:'red'}}
+            title="最热"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_polular.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image,{tintColor:'red'}]} source={require('./res/images/ic_polular.png')} />}
+            badgeText="1"
+            onPress={() => this.setState({ selectedTab: 'tb_polular' })}>
+            <View style={styles.page1}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_trending'}
+            selectedTitleStyle={{color:'yellow'}}
+            title="趋势"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_trending.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image,{tintColor:'yellow'}]} source={require('./res/images/ic_trending.png')} />}
+            // renderBadge={() => <CustomBadgeView />}
+            onPress={() => this.setState({ selectedTab: 'tb_trending' })}>
+            <View style={styles.page2}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_favorite'}
+            selectedTitleStyle={{color:'red'}}
+            title="收藏"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_favorite.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image,{tintColor:'red'}]} source={require('./res/images/ic_favorite.png')} />}
+            badgeText="1"
+            onPress={() => this.setState({ selectedTab: 'tb_favorite' })}>
+            <View style={styles.page1}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_my'}
+            selectedTitleStyle={{color:'yellow'}}
+            title="我的"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_my.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image,{tintColor:'yellow'}]} source={require('./res/images/ic_my.png')} />}
+            // renderBadge={() => <CustomBadgeView />}
+            onPress={() => this.setState({ selectedTab: 'tb_my' })}>
+            <View style={styles.page2}></View>
+          </TabNavigator.Item>
+        </TabNavigator>
       </View>
     );
   }
@@ -32,18 +77,30 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  page1:{
+    flex:1,
+    backgroundColor:'red',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  page2:{
+    flex:1,
+    backgroundColor:'yellow',
   },
+  image:{
+    width:22,
+    height:22
+  }
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10,
+  // },
+  // instructions: {
+  //   textAlign: 'center',
+  //   color: '#333333',
+  //   marginBottom: 5,
+  // },
 });
